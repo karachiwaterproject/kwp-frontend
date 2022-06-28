c1a8e8431223b01226681a3109bfdf16824dd3a2
Muhammad Jazzel Mehmood <jazzelmehmood4@gmail.com>
2022-06-29 01:58:20 +0500

    Fixed import issue (2)

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ import HomeNodeWeekly from "views/Node/HomeNodeWeekly";
 import AdminRoute from "routing/AdminRoute";
 import Blogs from "views/Blogs/Blogs";
 import AdminStaffRoute from "routing/AdminStaffRoute";
-import TestHomePage from "views/HomePage/TestHomePage";
 import Spinner from "Spinner";
 
 var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
@@ -106,7 +105,6 @@ const App = () => {
         {loading && <Spinner _height={`${height}vh`} />}
         <Switch>
           <Route path="/" component={HomePage} exact />
-          <Route path="/test" component={TestHomePage} exact />
           <Route path="/team" component={Teams} exact />
           <Route path="/extra" component={Teams} exact />
           <Route
